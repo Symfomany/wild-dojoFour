9545be85b3e838c4480034824d2179fcfe776c03
Symfomany <zuzu38080@gmail.com>
2018-03-20 09:30:30 +0100

    add training

---
+++ training.js
@@ -0,0 +1,7 @@
+// Fonctions de chaînes
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String
+
+// chaine -> tableau
+
+const mot = "Wilder";
+console.log(mot[0]);
