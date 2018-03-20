95e3036058a70eabe28ee708a6c63de48cbe8784
Symfomany <zuzu38080@gmail.com>
2018-03-20 09:28:45 +0100

    add changes for Palyndrome

---
+++ main.js
@@ -0,0 +1,13 @@
+class Palyndrome {
+  constructor(mot) {
+    this.mot = mot;
+  }
+
+  estPalyndrome() {
+    // You're code here...
+  }
+}
+
+const mot = "Kayak";
+const palyndrome = new Palyndrome();
+palyndrome.estPalyndrome(mot);
